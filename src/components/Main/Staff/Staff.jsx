d6a447c131d0a17bc2706e1384eb4444e7819f79
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-09-01 14:11:50 +0200

    add/useEffect

--- src/components/Main/Staff/Staff.jsx
+++ src/components/Main/Staff/Staff.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { DNA, FidgetSpinner, ProgressBar } from 'react-loader-spinner';
+import PokemonCard from "./PokemonCard";
 
 const Staff = () => {
-  return <div>Staff</div>;
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const resp = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await resp.json();
+
+      // Simulamos un retardo de 2 segundos
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+
+      setPokemons(data.results); // [] de pokemons de la API
+    };
+    getPokemons();
+  }, []);
+
+  const renderPokemons = () => {
+      return pokemons.map((pokemon, i) => <PokemonCard pokemon={pokemon} key={uuidv4()} />);
+  };
+
+  return <section className="staff">
+      {pokemons.length!=0? renderPokemons():<>
+          <DNA
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+          />
+
+          <FidgetSpinner
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="fidget-spinner-loading"
+            wrapperStyle={{}}
+            wrapperClass="fidget-spinner-wrapper"
+            />
+
+          <ProgressBar
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="progress-bar-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            />
+        </>
+    }
+  </section>
+
 };
 
 export default Staff;
--- src/components/Main/Topic/Topic.jsx
+++ src/components/Main/Topic/Topic.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import { ProgressBar } from 'react-loader-spinner';
+
 
 const Topic = () => {
-  return <div>Topic</div>;
+
+  const [value, setValue] = useState("holidays"); // Para guardar el dato a buscar
+  const [posts, setPosts] = useState([]); // Para guardar los posts
+  
+  // equivale a un componentDidUpdate()
+  useEffect(() => {
+    async function fetchData() {
+      try{
+        // Petición HTTP
+        const res = await axios.get(`https://www.reddit.com/r/${value}.json`);
+        const json = res.data;
+
+        // Guarda en el array de posts el resultado. Procesa los datos
+        const data = json.data.children.map(c => c.data);
+        setPosts(data);
+        
+      }catch(e){
+        setPosts([]) // No pintes nada 
+      }
+    }
+
+    fetchData();
+  }, [value]); // componentDidUpdate
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.topic.value)
+    setValue(e.target.topic.value) // Modificando el estado de Value
+  };
+
+
+  return    <section className="topic">
+              <h1>Topic</h1>
+              <form onSubmit={handleSubmit}>
+                <input name="topic"/>
+                <button>Buscar</button>
+              </form>
+
+              {posts.length!==0?
+                                <ul className='topics'>
+                                  {posts.map(post => (
+                                    <li key={post.id}>{post.title}</li>
+                                  ))}
+                                </ul>
+                                : 
+                                <>
+                                <p>Busca tu topic. No desistas si no ves resultados</p>
+                                <ProgressBar
+                                visible={true}
+                                height="80"
+                                width="80"
+                                color="#4fa94d"
+                                ariaLabel="progress-bar-loading"
+                                wrapperStyle={{}}
+                                wrapperClass=""
+                                />    
+                                
+                                </>
+                                           
+              }
+            </section>
 };
 
 export default Topic;
